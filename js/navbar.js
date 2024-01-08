e9f34267775e6950498efec7b4b8b27c93430e6f
DiEgOfercri <dfc25@icloud.com>
2024-01-08 12:55:32 +0100

    dropdowns navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,10 @@
 const nav = document.getElementById('nav');
 const toggleMenuOpen = document.getElementById('toggle-menu__open');
 const toggleMenuClose = document.getElementById('toggle-menu__close');
+const dropdownContent0 = document.getElementById('dropdown__content0');
+const dropdownToggle0 = document.getElementById('dropdown__toggle0');
+const dropdownContent1 = document.getElementById('dropdown__content1');
+const dropdownToggle1 = document.getElementById('dropdown__toggle1');
 const mainMenu = document.getElementById('main-menu');
 
 toggleMenuOpen.addEventListener('click', e => {
@@ -11,6 +15,14 @@ toggleMenuOpen.addEventListener('click', e => {
 toggleMenuClose.addEventListener('click', e => {
     nav.classList.toggle('nav--show');
     mainMenu.classList.toggle('main-menu--show');
+});
+
+dropdownToggle0.addEventListener('click', e => {
+    dropdownContent0.classList.toggle('dropdown__content--show');
+});
+
+dropdownToggle1.addEventListener('click', e => {
+    dropdownContent1.classList.toggle('dropdown__content--show');
 });
 
 window.addEventListener('resize',() => {
