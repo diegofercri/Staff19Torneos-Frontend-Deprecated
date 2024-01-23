f43950b47f155cc3d6c9eba9fce570a6ea862556
DiEgOfercri <dfc25@icloud.com>
2024-01-24 00:31:41 +0100

    slider js

---
+++ js/slide.js
@@ -0,0 +1,6 @@
+const bodyId = document.body.id;
+
+document.documentElement.style.setProperty('--image-1', 'url(../events/ended/' + bodyId + '/img/slide/1.jpg)');
+document.documentElement.style.setProperty('--image-2', 'url(../events/ended/' + bodyId + '/img/slide/2.jpg)');
+document.documentElement.style.setProperty('--image-3', 'url(../events/ended/' + bodyId + '/img/slide/3.jpg)');
+document.documentElement.style.setProperty('--image-4', 'url(../events/ended/' + bodyId + '/img/slide/4.jpg)');
