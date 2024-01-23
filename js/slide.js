d9aad41854040b35dd0f753a7d55d68d023af95d
DiEgOfercri <dfc25@icloud.com>
2024-01-24 00:59:34 +0100

    slide js end

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,34 @@
-const bodyId = document.body.id;
+// Obtienes el id del body
+var bodyId = document.body.id;
+console.log('Body ID:', bodyId);
+// Creas un nuevo elemento 'style'
+var style = document.createElement('style');
 
-document.documentElement.style.setProperty('--image-1', 'url(../events/ended/' + bodyId + '/img/slide/1.jpg)');
-document.documentElement.style.setProperty('--image-2', 'url(../events/ended/' + bodyId + '/img/slide/2.jpg)');
-document.documentElement.style.setProperty('--image-3', 'url(../events/ended/' + bodyId + '/img/slide/3.jpg)');
-document.documentElement.style.setProperty('--image-4', 'url(../events/ended/' + bodyId + '/img/slide/4.jpg)');
+// Añades la regla @keyframes al elemento 'style'
+style.innerHTML = `
+  @keyframes animate {
+    25% {
+        background-image: url(../${bodyId}/img/slide/4.jpg);
+    }
+    50% {
+        background-image: url(../${bodyId}/img/slide/3.jpg);
+    }
+    75% {
+        background-image: url(../${bodyId}/img/slide/2.jpg);
+    }
+    100% {
+        background-image: url(../${bodyId}/img/slide/1.jpg);
+    }
+  }
+
+  .slide {
+    background-size: cover;
+    height: 100%;
+    width: 100%;
+    border-radius: 10px;
+    animation: animate 10s infinite;
+  }
+`;
+
+// Añades el elemento 'style' al documento
+document.head.appendChild(style);
